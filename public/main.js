212450d63dfbdcb8a2eb6cd3a56aeb39fe17173b
Hugo <carnielhugo8@gmail.com>
2021-01-04 23:24:33 -0300

    she is answer me

--- public/main.js
+++ public/main.js
@@ -6,4 +6,15 @@ client.on("ready", () => {
   console.log(`Logado com o bot ${client.user.tag}`);
 });
 
+client.on("message", (message) => {
+  if (message.author == client.user) {
+    return;
+  } else {
+    if (message.content == "ola") {
+      message.reply("ola");
+    }
+    console.log(`${message.author.username}: ${message.content}`);
+  }
+});
+
 client.login(config.token);
